762d49bed5a3aa7588ab210b1f9aec589df5b918
adriton <adrtler@gmail.com>
2020-09-26 13:11:04 +0200

    Añadimos instalacion de big Calenadr y el calendario incial

--- src/index.js
+++ src/index.js
@@ -2,10 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
 
-
-ReactDOM.render(
-    <CalendarApp />,
-  document.getElementById('root')
-);
-
-
+import './styles.css';
+ReactDOM.render(<CalendarApp />, document.getElementById('root'));
