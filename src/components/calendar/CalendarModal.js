e5497c23e1cce7e30edb31dabd2491e07b231df7
adriton <adrtler@gmail.com>
2020-09-26 19:29:14 +0200

    Obtenemos la información del formulario del evento

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+
 import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment';
 const customStyles = {
   content: {
     top: '50%',
@@ -13,23 +15,118 @@ const customStyles = {
 };
 
 Modal.setAppElement('#root');
+
+const now = moment().minutes(0).seconds(0).add(1, 'hour');
+const end = moment().minutes(0).seconds(0).add(2, 'hour');
+
 export const CalendarModal = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  const closeModal = () => {
-    setIsOpen(false);
+  const [dateStart, setdateStart] = useState(now.toDate());
+  const [dateEnd, setDateEnd] = useState(end.toDate());
+
+  const [formValues, setformValues] = useState({
+    title: 'Evento',
+    notes: '',
+    start: now.toDate(),
+    end: end.toDate(),
+  });
+
+  const { notes, title } = formValues;
+
+  const handleInputChange = ({ target }) => {
+    setformValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
+  const closeModal = () => {};
+
+  const handleStartDateChange = (e) => {
+    setdateStart(e);
+    setformValues({
+      ...formValues,
+      start: e,
+    });
+  };
+  const handleEndDateChange = (e) => {
+    setDateEnd(e);
+    setformValues({
+      ...formValues,
+      end: e,
+    });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(formValues);
   };
   return (
     <Modal
-      isOpen={isOpen}
+      isOpen={true}
       onRequestClose={closeModal}
       style={customStyles}
       closeTimeoutMS={200}
       className="modal"
       overlayClassName="modal-fondo"
     >
-      <h1>Hola Mundo</h1>
+      <h1> Nuevo evento </h1>
       <hr />
-      <span>Hola de nuevo...</span>
+      <form className="container" onSubmit={handleSubmitForm}>
+        <div className="form-group">
+          <label>Fecha y hora inicio</label>
+          <DateTimePicker
+            onChange={handleStartDateChange}
+            value={dateStart}
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Fecha y hora fin</label>
+          <DateTimePicker
+            onChange={handleEndDateChange}
+            value={dateEnd}
+            minDate={dateStart}
+            className="form-control"
+          />
+        </div>
+
+        <hr />
+        <div className="form-group">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+            value={title}
+            onChange={handleInputChange}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className="form-group">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={notes}
+            onChange={handleInputChange}
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
   );
 };
