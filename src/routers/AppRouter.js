7f06e1c3cbbcec9ee26aa2c43e08ae67c2448718
adriton <adrtler@gmail.com>
2020-10-17 19:13:26 +0200

    Protección de las rutas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,28 +1,37 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
   Redirect,
 } from 'react-router-dom';
 import { startChecking } from '../actions/auth';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 export const AppRouter = () => {
   const dispatch = useDispatch();
+  const { checking, uid } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(startChecking());
   }, [dispatch]);
+
+  if (checking) {
+    return <h5>Espere...</h5>;
+  }
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/login">
-            <LoginScreen />
-          </Route>
-          <Route exact path="/" component={CalendarScreen}></Route>
+          <PublicRoute
+            exact
+            path="/login"
+            component={LoginScreen}
+            isLoggedIn={!!uid}
+          />
+          <PrivateRoute exact path="/" component={CalendarScreen} isLoggedIn={!!uid} />
           <Redirect to="/login" />
         </Switch>
       </div>
